0047dcbb989d2a6f27814cebe5945713af11a878
Jonas Johannsen <jonas.johannsen@haw-hamburg.de>
2016-11-22 09:23:32 +0100

    updated

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jones
+ * Date: 22.11.2016
+ * Time: 09:21
+ */
+
+namespace Source\Controller;
+
+
+class RegistrationController extends AbstractController
+{
+
+}
