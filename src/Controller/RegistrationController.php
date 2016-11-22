6f82354d098379056eb64cbcbaa8576388193def
Jonas Johannsen <jonas.johannsen@haw-hamburg.de>
2016-11-22 10:15:14 +0100

    working on the registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -7,12 +7,12 @@ class RegistrationController extends AbstractController
 
     public function index($request, $response, $args){
         //$response->getBody()->write("Registration-View in progress...");
-        return $this->ci->get('view')->render($response, 'signin.twig');
+        return $this->ci->get('view')->render($response, 'register.twig');
     }
 
 
     public function register($request, $response, $args){
         $response->getBody()->write(var_dump($request));
-        //return $this->ci->get('view')->render($response, 'signin.twig');
+        //return $this->ci->get('view')->render($response, 'register.twig');
     }
 }
