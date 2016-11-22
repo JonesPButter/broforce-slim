f9aa9e497f23152ea2e9f781b22e3753dcdb2eb6
Jonas Johannsen <jonas.johannsen@haw-hamburg.de>
2016-11-22 10:43:49 +0100

    the registration form works basically

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -1,6 +1,6 @@
 <?php
 namespace Source\Controller;
-
+use Source\Models\User;
 
 class RegistrationController extends AbstractController
 {
@@ -12,7 +12,22 @@ class RegistrationController extends AbstractController
 
 
     public function register($request, $response, $args){
-        $response->getBody()->write(var_dump($request));
+        $parsedBody = $request->getParsedBody();
+
+        $name = $parsedBody['name'];
+        $email = $parsedBody['email'];
+        $password_one = $parsedBody['password_one'];
+        $password_two = $parsedBody['password_two'];
+        // Email Validation: && !filter_var($email, FILTER_VALIDATE_EMAIL)
+        if($password_one == $password_two){
+            $hashed_password = password_hash($password_one, PASSWORD_DEFAULT);
+            $user = new User($name,$email,$hashed_password);
+            $response->getBody()->write(var_dump($user));
+        } else{
+            //hier muss noch eine flash Message angezeigt werden
+            $response->getBody()->write("Passwörter nicht identsich");
+        }
+
         //return $this->ci->get('view')->render($response, 'register.twig');
     }
 }
--- src/Models/User.php
+++ src/Models/User.php
@@ -9,5 +9,40 @@
 namespace Source\Models;
 class User
 {
+    private $name;
+    private $email;
+    private $password;
+
+    public function __construct($name, $email, $password)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
 
 }
