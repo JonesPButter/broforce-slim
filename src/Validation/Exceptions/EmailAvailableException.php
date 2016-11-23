46c0244c0722011834362b77917bc10a0744af6d
Jonas Johannsen <jonas.johannsen@haw-hamburg.de>
2016-11-23 01:50:11 +0100

    updated

---
+++ src/Validation/Exceptions/EmailAvailableException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jones
+ * Date: 23.11.2016
+ * Time: 01:22
+ */
+
+namespace Source\Validation\Exceptions;
+
+
+class EmailAvailableException
+{
+
+}
---
+++ src/Validation/Rules/EmailAvailable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jones
+ * Date: 23.11.2016
+ * Time: 01:17
+ */
+
+namespace Source\Validation\Rules;
+
+
+class EmailAvailable
+{
+
+}
