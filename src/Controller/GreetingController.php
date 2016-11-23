6140467a092931d2151c72f71c63c4563ed36da5
Jonas Johannsen <jonas.johannsen@haw-hamburg.de>
2016-11-23 02:02:32 +0100

    updated

--- src/Controller/GreetingController.php
+++ src/Controller/GreetingController.php
@@ -17,20 +17,4 @@ class GreetingController extends AbstractController
     public function greet($request, $response, $args) {
         $response->getBody()->write("Hello, World!");
     }
-
-    /*
-     * Um die funktion wieder verwenden zu können muss slim/php-view wieder required werden
-    public function greetWithName($request, $response, $args){
-        // Sample log message
-        $this->ci->get('logger')->info("Slim-Skeleton '/greeting/[{name}]' route");
-
-        // hier könnte man den COntainer zum args array dazu geben, damit wir
-        $routeURL =  $this->ci->get('router')->pathFor('greet');
-        // Routen rauskriegen: https://stackoverflow.com/questions/40505551/how-to-access-all-routes-from-slim-3-php-framework/40528450
-        // Render index view
-        return $this->ci->get('renderer')->render($response, 'index.phtml', [
-            'url' => $routeURL
-        ]);
-    }
-    */
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,37 +6,43 @@ use Respect\Validation\Validator as Validator;
 class RegistrationController extends AbstractController
 {
 
-    public function index($request, $response, $args){
-        //$response->getBody()->write("Registration-View in progress...");
-        //$messages = $this->ci->get('flash')->getMessages();
-        //$this->ci->get('view')->getEnvironment()->addGlobal('messages', $messages);
+    /**
+     * This is the index of the registration process,
+     * which shows the registration form.
+     */
+    public function index($request, $response){
         return $this->ci->get('view')->render($response, 'register.twig');
     }
 
 
-    public function register($request, $response, $args){
+    /**
+     * This function receives the data, typed in and submitted by the user
+     * at the register.twig-view.
+     */
+    public function register($request, $response){
         $validation = $this->ci->get('validator')->validate($request,[
             'email' => Validator::noWhitespace()->notEmpty()->emailAvailable(),
             'username' => Validator::noWhitespace()->notEmpty()->alpha(),
             'password' => Validator::noWhitespace()->notEmpty(),
         ]);
 
-        if(!$validation->failed()){
+        if($validation->failed()){
+            return $response->withRedirect($this->ci->get('router')->pathFor('register'));
+        } else{
             $user = User::create([
                 'username' => $request->getParam('username'),
                 'email' => $request->getParam('email'),
                 'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT),
             ]);
             return $response->withRedirect($this->ci->get('router')->pathFor('register.success'));
-        } else{
-            //$this->ci->get('flash')->addMessage('RegisterForm', 'Please fill in the whole form and check your passwords.');
-
-            return $response->withRedirect($this->ci->get('router')->pathFor('register'));
         }
     }
 
-    public function success($request, $response, $args){
-        //$response->getBody()->write("Hello");
+    /**
+     * This site shows up, when the user was successfully registered
+     * TODO Should be only viewed by a user, if he was really successfully registered
+     */
+    public function success($request, $response){
         return $this->ci->get('view')->render($response, 'registrationSuccess.twig');
     }
 }
