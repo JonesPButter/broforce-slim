5abd7e81367b500f4446e64323b22acf3675fc22
Jonas Johannsen <jonas.johannsen@haw-hamburg.de>
2016-12-06 11:17:27 +0100

    updated

--- src/Controller/ChangeUserDataController.php
+++ src/Controller/ChangeUserDataController.php
@@ -8,7 +8,6 @@
 
 namespace Source\Controller;
 
-
 class ChangeUserDataController extends AbstractController
 {
     public function getForm($request, $response){
@@ -19,27 +18,33 @@ class ChangeUserDataController extends AbstractController
 
     public function changeData($request, $response){
         $userID = $request->getAttribute('route')->getArgument('id');
-
         $name = $request->getParam('name');
         $familyname = $request->getParam('familyName');
         $email = $request->getParam('email');
 
         $user = $this->ci->get('userDAO')->getUserByID($userID);
+        $user->setName($name);
+        $user->setFamilyname($familyname);
 
-        if ($name != ''){
-            $user->setName($name);
-        }
-        if ($familyname != ''){
-            $user->setFamilyname($familyname);
-        }
-        if ($email != ''){
+        // die Email wird nur geändert, wenn sie nicht leer ist, es sich nicht um die alte Email handelt und
+        // es keinen anderen user mit derselben Email gibt.
+        if ($email != '' && $email != $user->getEmail() && ($this->ci->get('userDAO')->getUserWithEmail($email) === 0)){
             $user->setEmail($email);
+            $milliseconds = round(microtime(true) * 1000);
+            $token = $this->ci->get('auth')->createToken() . "-". $milliseconds;
+            $user->setToken($token);
+            $user->setVerified(false);
+            $url = $this->ci->get('router')->pathFor('verify',['userid'=>$userID, 'token'=>$token]);
+            $this->ci->get('flash')->addMessage('info',
+                'Please verify your new email address before your next login at https://broforce.informatik.haw-hamburg.de'. $url);
         }
-
         $user->setUpdatedAt(date('Y/m/d H:i:s'));
-
         $this->ci->get('userDAO')->updateUser($user);
         $this->ci->get('flash')->addMessage('info', 'The data was successfully updated');
-        return $response->withRedirect($this->ci->get('router')->pathFor('usertable'));
+        if($user->getRole() == 'admin'){
+            return $response->withRedirect($this->ci->get('router')->pathFor('usertable'));
+        } else{
+            return $response->withRedirect($this->ci->get('router')->pathFor('changeData', ['id' => $userID]));
+        }
     }
 }
